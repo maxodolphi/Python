5c8e5c38dc2bd95d2aebf7a51df97daed782a482
Max Odolphi <max.odolphi@gmail.com>
2024-12-05 16:38:53 +0100

    Eerste spelletje

---
+++ max.py
@@ -0,0 +1,70 @@
+import math
+import turtle
+
+#print(math.sqrt(4))
+#print('max is de leukste')
+#print('papa en max zijn de beste')
+
+#antwoord = input('Wie zijn de beste?')
+
+#print(f'{antwoord} zijn de beste')
+
+a = 4 * 6
+print(a * 100)
+a = 5 * (5 + 1) * 2
+print (a +10)
+a = 10 + (10 + 100) * 2
+print (a)
+print ('max' + 'max' 'max'*3 )
+l = [1,2,3,4,5,6,7,8,9,19,100,]
+x=sum(l)
+print (x)
+print(l[0:10])
+print("max"*10)
+
+def print_max(aantal):
+    print('max' * aantal)
+
+print_max(5)
+def print_sqrt(getal):
+    print(math.sqrt(getal))
+
+print_sqrt(9)
+
+def print_vaak(woorden, aantal):
+    print(woorden * aantal)
+
+kleur = "turquoise"
+print_vaak('Max ', 100)
+#turtle.pencolor(kleur)
+#turtle.begin_fill()
+#turtle.fillcolor(kleur)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.left(90) 
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(200)
+#turtle.left(360)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.end_fill()
+
+
+#turtle.mainloop()
+
+
+#print_vaak("papa en max en anna en mama ",2)
+
+#def print_letters(woord1, woord2):
+    #print(f'{woord1} {woord2} ' * 100)
+
+#print_letters('Max', 'Ernst')
+print_max(10)
+print_sqrt(36)
+print_max(100000000)
+q=("28*5")
+q
